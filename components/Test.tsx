8c0b4f5aad5da44e88f986450083c1205b4c478d
Paweł Naradowski <p.naradowski@gmail.com>
2023-06-11 19:45:04 +0200

    another try 5 and test with text

--- components/Test.tsx
+++ components/Test.tsx
@@ -6,7 +6,9 @@ const Test = () => {
       // initial={{ opacity: 0 }}
       // animate={{ opacity: 1 }}
       // transition={{ delay: 0.8, duration: 1 }}
-      className='w-[20vw] h-[20vw] bg-red-500 my-3'></motion.div>
+      className='w-[20vw] h-[20vw] bg-red-500 my-3'>
+      5
+    </motion.div>
   );
 };
 
