7056540ae7c70c921759ad8b103a8c4d68a9d5a9
Paweł Naradowski <p.naradowski@gmail.com>
2023-08-03 22:03:43 +0200

    Add initial animation prop in Gradient component

--- components/Gradient.tsx
+++ components/Gradient.tsx
@@ -1,24 +1,23 @@
 import { motion } from "framer-motion";
+import { useIntro } from "@/contexts/IntroContext";
 
 const Gradient = () => {
+  const intro = useIntro();
+
   return (
     <>
       <motion.div
         className='gradient-top'
-        initial={false}
+        initial={!intro ? { x: "-100%" } : false}
         animate={{ x: 0 }}
-        exit={{
-          x: "-100%",
-          transition: { duration: 0.75, ease: [1, 0, 0.8, 0.8] },
-        }}></motion.div>
+        exit={{ x: "-100%" }}
+        transition={{ duration: 0.75, ease: [1, 0, 0.8, 0.8] }}></motion.div>
       <motion.div
         className='gradient-bottom'
-        initial={false}
+        initial={!intro ? { x: "-100%" } : false}
         animate={{ x: 0 }}
-        exit={{
-          x: "-100%",
-          transition: { duration: 0.75, ease: [1, 0, 0.8, 0.8] },
-        }}></motion.div>
+        exit={{ x: "-100%" }}
+        transition={{ duration: 0.75, ease: [1, 0, 0.8, 0.8] }}></motion.div>
     </>
   );
 };
