df12d864be2a79b55d9f19118833d2f4b04da224
Paweł Naradowski <p.naradowski@gmail.com>
2023-08-03 16:02:41 +0200

    Refactor and simplify useScrollToTop hook

--- hooks/useScrollToTop.ts
+++ hooks/useScrollToTop.ts
@@ -1,14 +1,8 @@
 import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 
 export default function useScrollToTop(time: number) {
-  const path = usePathname();
-
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (path !== "/") document.body.classList.add("test");
-      window.scrollTo(0, 0);
-    }, time);
+    const timeout = setTimeout(() => window.scrollTo(0, 0), time);
     return () => clearTimeout(timeout);
   }, []);
 }
